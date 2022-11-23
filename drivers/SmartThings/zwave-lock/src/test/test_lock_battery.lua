eae9fae4e8ae0012505be7e57d0eeaf78f4110db
greens <steven.green@smartthings.com>
2022-11-23 14:06:25 -0800

    fixup

--- drivers/SmartThings/zwave-lock/src/test/test_lock_battery.lua
+++ drivers/SmartThings/zwave-lock/src/test/test_lock_battery.lua
@@ -118,21 +118,21 @@ test.register_coroutine_test(
 )
 
 test.register_coroutine_test(
-	"The driver should respond correctly to a time get",
-	function ()
-		test.socket.zwave:__queue_receive({ mock_device.id, Time:Get({},{
-				encap = zw.ENCAP.AUTO,
-				src_channel = 0,
-				dst_channels = {}
-			})
-		})
-		local time = os.date("*t")
-		test.socket.zwave:__expect_send(Time:Report({
-			hour_local_time = time.hour,
-			minute_local_time = time.min,
-			second_local_time = time.sec
-		}):build_test_tx(mock_device.id))
-	end
+  "The driver should respond correctly to a time get",
+  function ()
+    test.socket.zwave:__queue_receive({ mock_device.id, Time:Get({},{
+        encap = zw.ENCAP.AUTO,
+        src_channel = 0,
+        dst_channels = {}
+      })
+    })
+    local time = os.date("*t")
+    test.socket.zwave:__expect_send(Time:Report({
+      hour_local_time = time.hour,
+      minute_local_time = time.min,
+      second_local_time = time.sec
+    }):build_test_tx(mock_device.id))
+  end
 )
 
 test.run_registered_tests()
